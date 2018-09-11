6a5835b3ec830bdacf9da521469d9ed2b18ea727
aleng_zhang <aleng_zhang@outlook.com>
2018-09-11 20:58:15 +0800

    hotfix

--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ echo "欢迎使用termux一键配置脚本！"
 echo "正在进行安装...请稍后...\n正在更换软件源..."
 sed -ie '2cdeb https://mirrors.ustc.edu.cn/termux stable main' /data/data/com.termux/files/usr/etc/apt/sources.list
 
-echo "更换成功！更新中..."
-pkg up
+echo "Done！\n更新中..."
+apt update && apt upgrade -y
 
 echo "安装必备组件中..."
-pkg in git zsh vim
+apt install -y git zsh vim
 
 echo "正在进行 vim 配置&美化..."
 curl -fsSL https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > ~/.vimrc
+echo "Done!"
 
 echo "安装&配置 ohmyzsh"
 git clone https://github.com/aleng-zhang/Termux-Onekey.git "$HOME/Termux-Onekey" --depth 1
@@ -32,5 +33,5 @@ echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HO
 
 chsh -s zsh
 
-echo "Please restart Termux app..."
+echo "Done!\nPlease restart Termux app..."
 exit
