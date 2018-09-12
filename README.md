cd7fff8de941d11a553baebe5ead14947107cfda
aleng_zhang <aleng_zhang@outlook.com>
2018-09-12 12:25:21 +0800

    hot fix

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/aleng-zhang/Termux-Onekey/
 # 参考
 + [Cabbagec/termux-ohmyzsh](https://github.com/Cabbagec/termux-ohmyzsh)
 + [jerrita/termux](https://github.com/jerrita/termux)
+
+# 推荐 Termux 教程
++ [Termux 高级终端安装使用配置教程](http://www.sqlsec.com/2018/05/termux.html)
--- install.sh
+++ install.sh
@@ -1,25 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# -*- coding:utf-8 -*-
 
+clear
 termux-setup-storage
 
-echo "欢迎使用termux一键配置脚本！"
-echo "正在进行安装...请稍后...\n正在更换软件源..."
-sed -ie '2cdeb https://mirrors.ustc.edu.cn/termux stable main' /data/data/com.termux/files/usr/etc/apt/sources.list
+sed -ie '2cdeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main' "$PREFIX/etc/apt/sources.list"
 
-echo "Done！\n更新中..."
 apt update && apt upgrade -y
-
-echo "安装必备组件中..."
 apt install -y git zsh vim
 
-echo "正在进行 vim 配置&美化..."
 curl -fsSL https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > ~/.vimrc
-echo "Done!"
 
-echo "安装&配置 ohmyzsh"
 git clone https://github.com/aleng-zhang/Termux-Onekey.git "$HOME/Termux-Onekey" --depth 1
-cp "$HOME/Termux-Onekey/.termux" "$HOME/.termux"
+cp -R "$HOME/Termux-Onekey/.termux" "$HOME/.termux"
 rm -rf "$HOME/Termux-Onekey"
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
